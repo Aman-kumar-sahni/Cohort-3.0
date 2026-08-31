6932650cf9ef0b7655be1aff294956fa4233a90d
Aman Sahni <codetobuild125@gmail.com>
2026-08-31 07:21:12 +0530

    multistorage and diskstorage done

--- Backend/day-8/src/config/multer.js
+++ Backend/day-8/src/config/multer.js
@@ -1,16 +1,20 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
+const storageforlocal = multer.diskStorage({
 
     destination:(req,file,cb)=>{
 cb(null,"src/uploads/")
     },filename:(req,file,cb)=>{
+        //size aur format yhi check kr skte hai 
+
 
 cb(null,Date.now()+file.originalname)
     }
 
 })
 
-const upload = multer({storage})
+const storageforserver = multer.memoryStorage()
+
+const upload = multer({storage:storageforserver})
 
 module.exports =upload;
